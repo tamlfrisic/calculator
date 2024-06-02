b284a111a1e123a3ac40c291e8bd8f9eff79435e
tamlfrisic <tamlfrisic@gmail.com>
2024-06-02 13:34:57 -0500

    Add some variables and a bit of pseudocode

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let operand1;
+let operator;
+let operand2;
+let result;
+
+const calculator = document.querySelector("calc-body");
+const display = calculator.querySelector("display");
+const buttons = calculator.querySelectorAll("button");
+
+// if the button clicked is the clear button: 
+// clear.addEventListener("click", () => {
+//     operand1 = "";
+//     operand2 = "";
+//     operator = "";
+//     result = "";
+//     display.replaceChildren();
+//     
+// })
+
 function add(a, b) {
     return a + b;
 }
@@ -14,14 +33,35 @@ function divide(a, b) {
     return a / b;
 }
 
-const power = function(a, b) {
-    return a ** b;
+function percent(a) {
+    return a / 100;
 }
+
+function operate(operator, operand1, operand2) {
+    // but operate should only be called if the equals sign is pressed
+    // should i use something similar to the event listener and target.choice + 
+        // switch case as in EaS?
+    if (operator == "+") {
+        result = add(operand1, operand2);
+    } else if (operator == "-") {
+        result = subtract(operand1, operand2);
+    } else if (operator == "x") {
+        result = multiply(operand1, operand2);
+    } else if (operator == "/") {
+        result =divide(operand1, operand2);
+    } else if (operator == "%") {
+        result = percent(operand1);
+    }
+    return result;
+}
+
+
+// result becomes operand 1 
+//     however, if the clear is pressed, it restarts
+
 
 // = calculate
 // CE clear entry
 // AC all clear
-// sq. square root
 // % percentage
-// exponent (as button)
 // . button for floats/decimals (limit to once)
